6c9a50c78a906c35fb60eb7646fb93267c7d1f45
KYCb2 <kazekin99@gmail.com>
2021-06-02 14:52:07 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Pkg.go README
+
+Pkg.go is Extension for easy search Golang packages from ![pkg.go.dev](https://pkg.go.dev/)!
+
+## Features
+
+Extension uses for search package by name in Command Pallete and paste for example ("PackageName") to editor under cursor.
+
+## Example
+
+![Example Gif](https://github.com/KYCb2/PkgGoVsCode/blob/master/resources/feature.gif?raw=true)
+
+<!-- TODO: Setup gif in github! -->
+### 0.0.1
+
+First commit, created prototype.
+
+### 0.0.2
+
+Added README file.
+
+### 0.0.3
+
+Added Gif To README file.
+
+### For more information
+
+* [MyLinkedIn](https://www.linkedin.com/in/nikita-kazeka-432a00211/)
+* [MyGithub](https://github.com/KYCb2/GopherPkgSearchVsCode)
+
+**Enjoy Gophers!**
---
+++ extension.js
@@ -0,0 +1,105 @@
+const vscode = require('vscode');
+const rp = require('request-promise');
+const cheerio = require("cheerio");
+
+const url = "https://pkg.go.dev/search?q=";
+
+async function showInputBox() {
+	const result = await vscode.window.showInputBox({
+		value: '',
+		placeHolder: 'Search',
+	});
+	return `${result}`;
+}
+
+// TODO: Add import if import is initialized now
+function insertImport(pkg) {
+	const editor = vscode.window.activeTextEditor;
+
+	if (editor) {
+		const document = editor.document;
+		if (editor.selection.isEmpty) {
+			var position = editor.selection.active;
+			editor.edit(editBuilder => {
+				editBuilder.insert(position, `"${pkg}"`);
+			});
+		}
+	}
+}
+async function pickPackage(names, descs) {
+	let pkgs = [];
+	for (let i = 0; i < names.length; i++) {
+		let item = {
+			label: names[i],
+			description: descs[i]
+		};
+		pkgs.push(item);
+	}
+
+	const result = await vscode.window.showQuickPick(pkgs, {
+		placeHolder: 'Package..."undefined"'
+	});
+	insertImport(`${result}`);
+}
+
+async function getNames(q){
+	var resNames = [];
+	await rp(q)
+		.then(html => {
+			const $ = cheerio.load(html);
+			for (let i = 0; i < 10; i++) {
+				let name = $('.LegacySearchSnippet > .LegacySearchSnippet-header > a', html)[i].attribs.href.substring(1);
+				resNames.push(name);
+			}
+		});
+    return resNames;
+}
+
+async function getDescriptions(q) {
+	var resDescriptions = [];
+	await rp(q)
+		.then(html => {
+			const $ = cheerio.load(html);
+			for (let i = 0; i < 10; i++) {
+				let child = $('.LegacySearchSnippet > .SearchSnippet-synopsis', html)[i].childNodes[0];
+				if (child.data != undefined) {
+					resDescriptions.push(child.data)
+					continue;
+				}
+				resDescriptions.push("");
+			}
+		})
+		.catch(err => console.error(err));
+    return resDescriptions;
+}
+
+async function getPackagesWithDescription(query) {
+	let q = url + query;
+	let names = await getNames(q) 
+	let descs = await getDescriptions(q)
+	pickPackage(names, descs)
+}
+
+function activate(context) {
+
+	console.log('Congratulations, your extension "gophersearch" is now active!');
+
+	let disposable = vscode.commands.registerCommand('gophersearchjs.pkggodev', () => {
+		showInputBox().then(
+			value => {
+				console.log(value);
+				getPackagesWithDescription(value);				
+			}
+		);
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	// eslint-disable-next-line no-undef
+	deactivate
+}
